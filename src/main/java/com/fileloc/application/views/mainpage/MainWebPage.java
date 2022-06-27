5ba47b6e285fefce9a269050ffd411bcd6ea8815
Rumlor <edvestemhem@gmail.com>
2022-06-27 07:42:56 +0300

    second commit

---
+++ src/main/java/com/fileloc/application/views/mainpage/MainWebPage.java
@@ -0,0 +1,20 @@
+package com.fileloc.application.views.mainpage;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("list")
+@Route(value = "")
+public class MainWebPage extends VerticalLayout {
+
+    public MainWebPage() {
+        setSpacing(false);
+
+
+    }
+
+}
