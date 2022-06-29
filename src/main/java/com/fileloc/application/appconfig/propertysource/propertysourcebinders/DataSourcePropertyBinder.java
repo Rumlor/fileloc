16a31beeb725ca5e0d7feca038abc48fb21cb158
Rumlor <mehmet.dogan@dekatechs.com>
2022-06-29 14:09:48 +0300

    data source config

--- src/main/java/com/fileloc/application/appconfig/propertysource/propertysourcebinders/DataSourcePropertyBinder.java
+++ src/main/java/com/fileloc/application/appconfig/propertysource/propertysourcebinders/DataSourcePropertyBinder.java
@@ -4,17 +4,16 @@ import com.fileloc.application.appconfig.propertysource.DataSourceProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
-@Configuration
+
 @ConfigurationProperties(prefix = "app.data")
+@Component
 @PropertySource(value ="classpath:datasourceconfig.yml" ,factory = DataSourceProperty.class)
 @Getter
 @Setter
 public class DataSourcePropertyBinder {
-
-
     private String driver;
     private String url;
     private String username;
