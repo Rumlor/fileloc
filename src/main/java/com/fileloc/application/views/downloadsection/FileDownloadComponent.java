3e936f33f2da109df97af533a9a9e547aa706034
Rumlor_new <edvestemhem@gmail.com>
2022-07-03 18:32:52 +0300

    file download functionality added

--- src/main/java/com/fileloc/application/views/downloadsection/FileDownloadComponent.java
+++
@@ -1,7 +0,0 @@
-package com.fileloc.application.views.downloadsection;
-
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-
-public class FileDownloadComponent extends VerticalLayout {
-
-}
